f09b1bf8d1e5531b1e86f036d0585a7a0664f1c5
Adityas249 <you@example.com>
2020-01-12 23:59:13 +0530

    cryptowiki

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+import SearchForm from './Components/SearchForm';
+
+
+
+
+
+
+export default class App extends Component{
+
+
+
+
+    render(){
+
+        return(
+            <div className = "App">  
+
+
+
+
+            <SearchForm />
+            </div>
+
+
+
+        )
+
+
+
+    }
+    
+
+
+
+}
